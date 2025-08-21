9a27aeb613339b9824bb03c9b96704a62acfc3f9
root <root@bolt-vnasj3nzvz-s9hszzbqsh.bolt-pods.turi-bolt.svc.cluster.local>
2025-08-21 06:53:01 +0000

    3b sync

--- training/run_multi_domain.sh
+++ training/run_multi_domain.sh
@@ -6,7 +6,7 @@ RETAIL_VAL_DIR="data/tau_bench_retail"
 
 POLICY_MODEL="Qwen/Qwen2.5-3B-Instruct"
 REF_MODEL="Qwen/Qwen2.5-3B-Instruct"
-MODEL_NAME_SANITIZED=$(echo $POLICY_MODEL | tr '/' '_')_v7-multi-domain
+MODEL_NAME_SANITIZED=$(echo $POLICY_MODEL | tr '/' '_')_v8-multi-domain
 
 # Run training with multi-domain data and retail validation set
 bash training/run_tau_bench.sh \
